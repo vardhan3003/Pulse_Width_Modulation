082aec85dbc7d1c4eb7f2e6506e0cbd4d76c86c4
Harshavardhan Babu <139210570+vardhan3003@users.noreply.github.com>
2024-03-20 19:43:12 +0530

    Update pwm.v

--- pwm.v
+++ pwm.v
@@ -1,17 +1,12 @@
 `timescale 1ns / 1ps
-
  
-module pwm(
-input clk, rst, 
-output reg dout
-);
+module pwm(input clk, rst, output reg dout);
  
 parameter period = 100;
 integer count = 0;
-integer ton   = 0;
-reg nc      = 1'b0;
+integer ton  = 0;
+reg ncyc = 1'b0;
 reg key=1'b0;
-
  
 always@(posedge clk)
 begin
@@ -19,7 +14,7 @@ begin
         begin
          count <= 0;
          ton   <= 0;
-         nc  <= 1'b0;
+         ncyc  <= 1'b0;
         end   
      else 
        begin
@@ -27,17 +22,17 @@ begin
               begin
               count <= count + 1;
               dout  <= 1'b1;
-              nc  <= 1'b0;
+              ncyc  <= 1'b0;
               end
             else if (count < period)
               begin
               count <= count + 1;
               dout <= 1'b0;
-              nc <= 1'b0;
+              ncyc <= 1'b0;
               end
             else
                begin
-               nc  <= 1'b1;
+               ncyc  <= 1'b1;
                count <= 0;
                end
        end
@@ -48,19 +43,19 @@ always@(posedge clk)
 begin
      if(rst == 1'b0) 
      begin 
-             if(nc == 1'b1)
+           if(ncyc == 1'b1)
                 begin
                     if(key == 1'b1 && ton==0) begin
                        key <= 0;
                         ton <= 0;
                        end
                      else if (key==0 && ton<period) begin
-                       key<=0;
+                       key <= 0;
                        ton <= ton+5;
                        end
                      else begin
-                     key<=1;
-                     ton=ton-5;
+                     key <= 1;
+                     ton <= ton-5;
                      end
                 end
      end   
